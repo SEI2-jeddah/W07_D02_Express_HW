feea919e83719294529847a24476d3eda3747b64
abdullah alsharif <abdullahalsharif@abdullahs-MacBook-Air.local>
2019-11-21 17:10:22 +0300

    server hw

---
+++ first_server/server.js
@@ -0,0 +1,15 @@
+const express = require('express'); //from documentation: express is function
+const app = express();//app is an object
+const mongoose = express("mongoose")
+
+app.get('/', (req, res)=>{
+  res.send('Hello world');
+});
+
+app.get('/somedata', (req, res) => {
+    res.send('here is your information');
+});
+
+app.listen(3000, () => {
+    console.log("I am listening");
+});
